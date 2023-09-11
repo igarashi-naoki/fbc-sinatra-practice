66fb904908d0c501a6838a24b9f2d83baf83c45b
igarashi-naoki <naoki17igarashi@gmail.com>
2023-09-11 09:02:24 +0900

    プレースホルダによるSQLインジェクション対策を追加

--- memo.rb
+++ memo.rb
@@ -13,19 +13,19 @@ class Memo
 
   class << self
     def create(memo_id:, title:, body:)
-      @db_connect.exec("INSERT INTO memos(memo_id,title,body) values('#{memo_id}','#{title}','#{body}');")
+      @db_connect.exec_params('INSERT INTO memos(memo_id,title,body) values($1,$2,$3);', [memo_id, title, body])
     end
 
     def read(memo_id:)
-      @db_connect.exec("SELECT * FROM memos WHERE memo_id = '#{memo_id}';")
+      @db_connect.exec_params('SELECT * FROM memos WHERE memo_id = $1;', [memo_id])
     end
 
     def update(memo_id:, title:, body:)
-      @db_connect.exec("UPDATE memos SET title = '#{title}', body = '#{body}' WHERE memo_id = '#{memo_id}';")
+      @db_connect.exec_params('UPDATE memos SET title = $2, body = $3 WHERE memo_id = $1;', [memo_id, title, body])
     end
 
     def delete(memo_id:)
-      @db_connect.exec("DELETE FROM memos WHERE memo_id = '#{memo_id}';")
+      @db_connect.exec_params('DELETE FROM memos WHERE memo_id = $1;', [memo_id])
     end
 
     def connect_db
