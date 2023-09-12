73b267bc4d52b2ab51c131d39903ae3194fde8df
igarashi-naoki <naoki17igarashi@gmail.com>
2023-09-12 22:30:33 +0900

    memosテーブルの作成を自動的に行うようにする

--- memo.rb
+++ memo.rb
@@ -7,30 +7,41 @@ class Memo
   attr_reader :title, :body
 
   @db_connect = nil
+  @table_name = 'memos'
 
   class << self
     def create(title:, body:)
-      @db_connect.exec_params('INSERT INTO memos(title,body) values($1,$2);', [title, body])
+      @db_connect.exec_params("INSERT INTO #{@table_name}(title,body) values($1,$2);", [title, body])
     end
 
     def read(id:)
-      @db_connect.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
+      @db_connect.exec_params("SELECT * FROM #{@table_name} WHERE id = $1;", [id])
     end
 
     def update(id:, title:, body:)
-      @db_connect.exec_params('UPDATE memos SET title = $2, body = $3 WHERE id = $1;', [id, title, body])
+      @db_connect.exec_params("UPDATE #{@table_name} SET title = $2, body = $3 WHERE id = $1;", [id, title, body])
     end
 
     def delete(id:)
-      @db_connect.exec_params('DELETE FROM memos WHERE id = $1;', [id])
+      @db_connect.exec_params("DELETE FROM #{@table_name} WHERE id = $1;", [id])
     end
 
     def connect_db
       @db_connect = PG.connect(dbname: 'memo_app')
+
+      # memosテーブルが存在しなければ自動で作成する
+      @db_connect.exec("SELECT 1 FROM #{@table_name} LIMIT 1;")
+    rescue PG::UndefinedTable
+      @db_connect.exec("CREATE TABLE #{@table_name}(
+          id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+          title VARCHAR(200) NOT NULL,
+          body TEXT NOT NULL,
+          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+        );")
     end
 
     def read_all
-      @db_connect.exec('SELECT * FROM memos ORDER BY created_at DESC;')
+      @db_connect.exec("SELECT * FROM #{@table_name} ORDER BY created_at DESC;")
     end
   end
 
